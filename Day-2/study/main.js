70e3c88cfb5ac444416eb83f323fb96865b321d4
Rkulkrni <rahulspk7@gmail.com>
2026-02-10 10:57:59 +0530

    Day2-Variables

---
+++ Day-2/study/main.js
@@ -0,0 +1,53 @@
+// comment 
+
+/* kjsbdbdfnrj
+jsdbdsncd
+sdjbc dsc dsc
+kmsndfkds fdk'
+kjsdnfkdsjnf
+kjsdfsdjnf'
+dsnf ds f */
+
+// Variable : 
+
+// var, let, const
+
+/*
+var : 
+- gobal scope.
+- function scope.
+- can be re-declared.
+- can be updated/re-assigned.
+*/
+var a = 10;
+// console.log(a);
+a = 20;
+console.log(a);
+
+var a = true;
+console.log(a);
+
+/*let : 
+- block scope.
+- cannot be re-declared.
+- can be updated/re-assigned.
+*/
+let b = 10;
+console.log(b);
+b = 20;  // reassign
+console.log(b);
+// let b = true; // cannot be re-declared
+// console.log(b);
+
+/*const :
+- block scope.
+- cannot be re-declared.
+- cannot be updated/re-assigned.
+*/
+
+const c = 10;
+console.log(c);
+// c = 20; // cannot be re-assigned
+// console.log(c);
+// const c = true; // cannot be re-declared
+// console.log(c);
