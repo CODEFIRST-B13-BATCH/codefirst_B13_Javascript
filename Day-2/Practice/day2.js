83ecf678c8edf93647292e7671f4402673d4f915
Sanket-777-Yewale <sanketyewale777@gmail.com>
2026-02-11 11:08:13 +0530

    day to practice

---
+++ Day-2/Practice/day2.js
@@ -0,0 +1,5 @@
+var test1 = "CTS Pune";
+ // test1 = 55;
+var test2 = 35;
+ console.log(typeof(test1));
+ console.log(typeof(test2));
--- Day-2/Practice/main.js
+++ Day-2/Practice/main.js
@@ -1,3 +1,33 @@
-console.log("Hellow")
+/*
+let x = 10;
+let y = 10;
 
-console.log("hello sanket")
+let sum = x+y;
+let product = x*y;
+
+console.log("sum:",sum);
+console.log("product:",product);
+
+
+let name = "sanket";
+name = 22;
+console.log(name,name);
+*/
+
+// const user={
+//     name : "sanket",
+//     age : 22
+// };
+// user.age=26;
+// user.city="pune";
+
+// console.log(user);
+
+let x = 10;
+console.log(++x);
+console.log(x++);
+
+
+
+
+
