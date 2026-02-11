7215415d0bb8b3f3787a386e16ccf42364b7ea42
AVADHUT9279 <avadhutpatil9279@gmail.com>
2026-02-11 11:13:53 +0530

    Day 2 Practice

---
+++ Practice/variable.js
@@ -0,0 +1,15 @@
+ console.log("hello");
+        let a=10;
+        console.log("variable",a);
+        b= 20;
+        console.log("let variable",a);
+
+        var b=20;
+        console.log("var Variable",b);
+        b=10;
+
+        console.log("var Variable",b);
+
+        const c=20;
+        console.log("Const Variable",c);
+     
