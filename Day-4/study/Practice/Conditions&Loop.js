9267827b6fce024c0389486d8c502236db04577f
AVADHUT9279 <avadhutpatil9279@gmail.com>
2026-02-12 17:50:43 +0530

    Conditions & Loops

---
+++ Day-4/study/Practice/Conditions&Loop.js
@@ -0,0 +1,170 @@
+/*
+if
+if else
+switch
+*/
+
+
+/*
+if(true){
+    console.log("This is true condition");
+}
+
+
+let a=10;
+let b=20
+if(a==b){
+    console.log("equal value");
+
+}
+else{
+    console.log("not equal value")
+}
+
+*/
+
+/*
+let day=2;
+switch(day){
+    case 1:
+        console.log("one");
+        break;
+
+    case 2:
+        console.log("Two");
+        break;
+
+    case 3:
+        console.log("Three");
+        break;
+
+    case 4:
+        console.log("Four");
+        break;
+
+    case 5:
+        console.log("Five");
+        break;
+
+    case 6:
+        console.log("Six");
+        break;
+
+    case 7:
+        console.log("Seven");
+        break;
+}
+*/
+
+
+/*
+Loop
+while lopp
+do while loop
+for loop
+*/
+
+/*
+let n=10;
+console.log("Reverse Number");
+while(n>0){
+    console.log(n);
+    n--;
+}
+    */
+
+
+/*
+let n=1;
+console.log("1 to 10 number::")
+while(n<=10){
+    console.log(n);
+    n++;
+}
+*/
+
+
+/*
+let n=1;
+do{
+    console.log(n);
+    n++;
+}while(n<=10);
+
+*/
+
+/*
+let n=10;
+do{
+    console.log(n);
+    n--;
+}while(n>=1);
+
+*/
+
+/*
+for(let i=1;i<=10;i++){
+    console.log(i);
+}
+*/
+
+
+/*
+for(let i=10;i>=1;i--){
+    console.log(i);
+}
+    */
+
+// Questions
+
+/*
+// 1. Write a program to check if a number is even or odd.
+let n=3;
+if(n %2===0){
+    console.log("Even");
+}
+else{
+    console.log("Odd");
+}
+*/
+
+/*
+// 2. Write a program to find the largest of two numbers.
+let n1=167;
+let n2=20;
+if(n1===n2){
+    console.log("They are equal");
+}
+else if(n1>n2){
+    console.log("n1 is greater");
+}
+else{
+    console.log("n2 is greater");
+}
+*/
+
+
+/*
+// 3. Write a program to check if a number is positive, negative, or zero.
+let n=3;
+if(n===0){
+    console.log("Number is Zero");
+}
+else if(n>0){
+    console.log("Number is positive");
+}
+else{
+    console.log("Number is negative");
+}
+*/
+
+/*
+//4. Write a program to check if a character is a vowel or consonant.
+let n='h';
+if(n==='a' || n==='e' || n==='i' || n==='o' || n==='u'){
+    console.log("vowel character")
+}
+else {
+    console.log("not vowel");
+}
+    */
