2c5a84e3a9b2b6a5f216b8a7c9a02f1b91b9d735
SHIVANI JADHAV <159504727+shivaniaj2@users.noreply.github.com>
2026-02-13 12:47:45 +0530

    Practice Question

---
+++ Day-4/practice/numIsPositiveNegativeOrZero.js
@@ -0,0 +1,9 @@
+let num=-7;
+if(num>0){
+    console.log(num+" is a Positive Numer");
+}else if(num==0){
+    console.log(num+" is a zero Number");
+
+}else{
+    console.log(num+" is a Negative Number");
+}
