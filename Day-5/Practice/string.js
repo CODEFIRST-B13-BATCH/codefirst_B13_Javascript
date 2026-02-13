7aff324d40bade9ac1670fe2e60d527270b673da
AVADHUT9279 <avadhutpatil9279@gmail.com>
2026-02-13 16:06:54 +0530

    string & string methods

---
+++ Day-5/Practice/string.js
@@ -0,0 +1,61 @@
+/*
+let str="Avadhut patil";
+
+// Methods
+
+// 1. length
+console.log(str.length);
+// 2. toUpperCase()
+console.log(str.toUpperCase());
+// 3. toLowerCase()
+console.log(str.toLowerCase());
+// 4. indexOf()
+console.log(str.indexOf("pa"));
+// 5. includes()
+console.log(str.includes("Ap"))
+// 6. slice()
+console.log(str.slice(0,5));
+// 7. split()
+console.log(str.split(" "))
+// 8. replace()
+console.log(str.replace("patil","PATIL"));
+// 9. trim()
+console.log(str.trim());
+// 10. charAt()
+console.log(str.charAt(1));
+// 11. search()
+console.log(str.search("A"));
+// 12. concat()
+let str1="From Helewadi";
+console.log(str.concat(" ",str1));
+
+*/
+// questions for practice :
+// 1. Create a string variable and use the length property to find out how many characters it contains.
+let str2="Avadhut";
+console.log(str2.length);
+// 2. Create a string variable and convert it to uppercase using the toUpperCase() method.
+console.log(str2.toUpperCase());
+// 3. Create a string variable and convert it to lowercase using the toLowerCase() method. 
+console.log(str2.toLowerCase()); 
+// 4. Create a string variable and use the indexOf() method to find the position of a specific word in the string.
+console.log(str2.indexOf("d"));
+// 5. Create a string variable and use the includes() method to check if a specific word is present in the string.
+console.log(str2.includes("v")); // return true value
+console.log(str2.includes("p")); // return false value
+// 6. Create a string variable and use the slice() method to extract a portion of the string.
+
+// 7. Create a string variable and use the split() method to split the string into an array of words.
+console.log(str2.split(""));
+// 8. Create a string variable and use the replace() method to replace a specific word with another word.
+let str3="AVADHUT";
+console.log(str2.replace(" ",str3));
+// 9. Create a string variable with leading and trailing whitespace and use the trim() method to remove the whitespace.
+console.log(str2.trim());
+// 10. Create a string variable and use the charAt() method to find the character at a specific index.
+console.log(str2.charAt(2));
+// 11. Create a string variable and use the search() method to find the position of a specific word in the string.
+console.log(str2.search("d"));
+// 12. Create two string variables and use the concat() method to combine them into a single string.
+let str4="patil";
+console.log(str2.concat(" ",str3));
