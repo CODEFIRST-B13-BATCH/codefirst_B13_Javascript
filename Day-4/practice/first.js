980fa2659ef37a45fc03e8da3f02da7ac179580f
SHIVANI JADHAV <159504727+shivaniaj2@users.noreply.github.com>
2026-02-13 11:14:39 +0530

    this is my first day to fourth day practice

---
+++ Day-4/practice/first.js
@@ -0,0 +1,157 @@
+console.log("Hello World!");
+console.log("Welcome to JavaScript");
+
+console.log("This is a simple JavaScript program.");
+
+let a=10;
+console.log (a);
+
+let b=20;
+console.log (b);
+
+let string="JavaScript";
+console.log(string);
+
+const c=1;
+console.log(c);
+const d=2;
+console.log(d);
+
+var e=5;
+console.log(e);
+var f=6;
+console.log(f);
+var f=7;
+console.log(f);
+
+ a=4;
+ b=5;
+ console.log(a+b);
+
+ let x=15;
+ let y=25;
+ console.log("Sum of x and y is "+(x+y));
+
+
+ var z=50;
+ var v=20;
+ console.log("Subtraction of z and v is "+(z-v));
+
+ const l=7;
+ const p=3;
+ console.log("Multiplication of l and p is "+(l*p)); 
+
+
+ let String1="Hello Shivani";
+ let String2="Welcome to JavaScript";
+ console .log(String1+" "+String2);
+
+ var add=10+20;
+ console.log(add);
+
+ var sub=50-20;
+ console.log(sub);
+
+ let num1=10;
+ let num2=5;
+ let mul =num1*num2;
+ console.log(mul);
+
+
+ var seven ;
+ console.log(seven);
+
+let r="Hello"+" "+3;
+console.log(typeof(r));
+console.log(r);
+
+
+// this is my first day in javascript class
+/*hhnnnjjnn
+ffvjjfj
+njjnnn*/
+
+let m=10;
+console.log(++m);
+
+let n=20;
+console.log(--n);
+
+
+let s=10;
+console.log(s++);
+
+let t=20;
+console.log(t--);
+
+const Q=false;
+console.log(!false);
+
+
+const R=false;
+console.log(typeof(R));
+
+
+const Data=(4>=5);
+console.log(Data);
+
+
+const Data2 = (3 > 4) ;
+console.log(Data2)
+
+const Data3=(4<5)?true:false;
+
+
+let Num1=2;
+const Num2=3;
+Num1+=Num2;
+console.log(Num1)
+
+// And operator
+let num3=10;
+let num4=7;
+console.log("num3 & num4 = "+(num3 & num4));
+
+console.log(4 & 6);
+
+
+// OR Operator
+let num5=12;
+let num6=14;
+console.log("num5 |num6 = "+(num5 | num6));
+
+// XOR Operator
+let num7=8;
+let num8=17;
+console.log("num7 ^ num8 = "+(num7 ^ num8));
+
+
+let num9=5;
+let num10='5';
+console.log(num9==num10);
+console.log(num9===num10);
+console.log(num9!=num10);
+console.log(num9!==num10);
+console.log(num9<=num10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
