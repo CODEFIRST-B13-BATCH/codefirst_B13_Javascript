f671520a4b562d831a6c39241a746e820a7c84c4
dev-AkshayMasal <akshaymasal35@gmail.com>
2026-02-25 00:10:19 +0530

    arr & obj que task complete

--- Day-5/answer.js
+++ Day-5/answer.js
@@ -70,7 +70,80 @@ console.log(person.name, person.hobbies[0]);
 
 // 6. Create an array of numbers and calculate the sum of all the numbers in the array. Then, log the sum to the console.
 
-const num = [1,2,3,4,5,6,7,8,9,11];
+const num = [1,2,3,4,5,6,7,8,9];
 const sum = num.reduce((total, num) => total + num, 0);
 
 console.log(sum);
+
+// 7. Create an object representing a company with properties like name, industry, and employees (which should be an array of objects representing individual employees). Then, log the company's name and the name of the first employee to the console.
+
+const company = {
+    name: 'Tata',
+    industry: 'IT',
+    employees:[{id: 101010},{id:202020},{id:303030}]
+    // [{id: 101010, post: 'junior'},{id:202020, post: 'junior'}, {id: 303030, post: 'senior'}]
+}
+console.log(`company name: ${company.name}, first employee: ${company.employees[0].id}`);
+
+
+
+// 8. Create an array of objects representing different countries, with properties like name, population, and capital. Then, log the name of the country with the largest population to the console.
+
+const country = [
+    {name: 'India', population: '150cr', capital: 'Delhi'},
+    {name: 'Israel', population: '5cr', capital: 'Jerusalem'},
+    {name: 'Rasia', population: '50cr', capital: 'Moscow'},
+]
+
+// with for loop 
+const populatedCountry = country[0];
+
+for ( i=0; i<populatedCountry.length; i++) {
+    if(country[i].population <= populatedCountry.population)
+        {
+            populatedCountry = country[i];
+    }
+}
+
+console.log(`largest populated country is ${populatedCountry.name}.`);
+
+// with reduce method
+const populatedCountries = country.reduce((max, country) =>
+  country.population < max.population ? country : max
+);
+
+console.log("country with largest population:", populatedCountries.name);
+
+
+// 9. Create an object representing a recipe with properties like name, ingredients (which should be an array), and instructions. Then, log the recipe's name and the first ingredient to the console.
+
+const recipe = {
+    name: "Pulav",
+    ingredient: ['Rice', 'Potato', 'Paneer', 'Greenchilli']
+}
+
+console.log(`It is a ${recipe.name} recipe & first ingredient is ${recipe.ingredient[0]}.` );
+
+
+// 10. Create an array of objects representing different cars, with properties like make, model, year, and color. Then, log the make and model of the car that was manufactured in the year 2020 to the console.
+
+
+const cars = [
+    {make: "Tata", model: "Fortuner", year: 2021, color:"white"},
+    {make: "Maruti", model: "Ciaz", year: 2020, color:"White"},
+    {make: "Ford", model: "Polo", year: 2022, color:"Red"},
+    {make: "Honda", model: "Amaze", year: 2020, color:"Black"},
+    {make: "Hyundai", model: "i10", year: 2023, color:"Yellow"},
+]
+
+// find method --> return only one object
+// const cars1 = cars.find(cars1 => cars1.year === 2020);
+// if(cars1){
+//     console.log(`manufactured in year 2020 cars is ${cars1.make} & model is ${cars1.model}.`);
+// }
+
+// filter method --> returns all the matching values
+const cars2 = cars.filter(car2 => car2.year === 2020);
+if(cars2){
+    console.log(`manufactured in year 2020 cars is ${cars2.make} & model is ${cars2.model}.`);
+}
