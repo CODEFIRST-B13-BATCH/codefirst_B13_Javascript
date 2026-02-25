05a0f774a3ba1b1c75a40589a70609ad1e4b1577
gayathri <you@example.com>
2026-02-25 17:23:17 +0530

    arrays example

--- Day-6/Study/examples.js
+++ Day-6/Study/examples.js
@@ -1,123 +1,97 @@
-let cart = [
-  { name: "Laptop", price: 50 },
-  { name: "Mouse", price: 5 },
-  { name: "Keyboard", price: 5000 }
+ let a = [10, 20, 30];
+ a.push(40);
+  console.log(a);
+
+  let b = ["apple", "banana", "mango"];
+  b.pop();
+  console.log(b);
+  
+  let c = ["middle", "end"]
+  c.unshift("start");
+  console.log(c);
+  
+  let d =[100, 200, 300];
+  d.shift(0);
+  console.log(d);
+
+  let e = ["html", "css", "js", "react"];
+   console.log(e.indexOf( "react"));
+
+   let f = [10, 20, 30, 40, 50];
+   let g = f.includes(60);
+   console.log(g);
+   
+  let h = [1,2,3,4,5];
+ console.log(h.splice(3,99));
+
+ let  i = [10,20,30,40,50];
+ const j =  i.slice(1,3)
+ console.log(j);
+ 
+ let l =["2026", "02", "24"];
+ const arr = l.join(" - ");
+ console.log(arr);
+ 
+let k = [2,4,6,8];
+let m = k.map((Element)=> Element*5);
+console.log(m);
+
+let n = [50,120,300,80,200];
+let o = n.filter((Element) => Element > 100);
+console.log(o);
+
+let p = [
+ {name:"A", age:25},
+ {name:"B", age:40},
+ {name:"C", age:35}
 ];
 
-let sum = cart.reduce((acc,currentValue) => {
-    return acc+ currentValue.price;
+const q = p.find((Element) => Element.age > 30);
+console.log(q);
 
-},0);
-console.log(sum);
 
-// Remove Mouse
-const arr = cart.filter((element)=> element.price > 5);
-console.log(arr);
+students = [
+  { name: "Amit", marks: 45 },
+  { name: "Riya", marks: 80 },
+  { name: "Sam", marks: 30 },
+  { name: "John", marks: 70 }
+]
+let s = students.filter((Element) => Element.marks > 50);
+console.log(s);
 
-//Increase all prices by 10% (GST)
-let upadated = cart.map(item =>{
-    return {
-        name:item.name,
-           price :item.price * 1.10
-    };
+na = [
+  { id: 1, name: "Gayatri" },
+  { id: 2, name: "Rahul" },
+  { id: 3, name: "Sneha" }
+]
+const t = na.find((elemnt)=> elemnt.id == 2);
+console.log(t);
+
+let u = [5000, 3000, 2000, 4000];
+const r = u.reduce((acc,current) =>{
+    return acc + current;
 });
-console.log(upadated);
+console.log(r);
 
-
-let students = [
-  { name: "A", marks: 35 },
-  { name: "B", marks: 80 },
-  { name: "C", marks: 60 },
-  { name: "D", marks: 20 }
-];
-//Get only passed students (>= 40)
-const arr2 = students.filter((item) => item.marks > 40);
-console.log(arr2);
-
-// Add grade property based on marks
-// Rules:
-// =75 → Distinction
-
-// =60 → First
-
-// =40 → Pass
-
-// else → Fail
-
-let grade = students.map(item =>{
-    return{ marks : item.marks,
-
-    };
-    });
-    console.log(grade);
-    
- var num = 153; // put your number here
-var temp = num;
-var add = 0;
-
-var power = num.toString().length;
-
-while (temp > 0) {
-  var digit = temp % 10;
-  add += digit ** power;
-  temp = Math.floor(temp / 10);
-}
-
-if (add === num) {
-  console.log(`${num} is an Armstrong number`);
-} else {
-  console.log(`${num} is not an Armstrong number`);
-}
-let cart = [
-  { name: "Laptop", price: 50 },
-  { name: "Mouse", price: 5 },
-  { name: "Keyboard", price: 5000 }
-];
-
-let sum = cart.reduce((acc,currentValue) => {
-    return acc+ currentValue.price;
-
-},0);
-console.log(sum);
-
-// Remove Mouse
-const arr = cart.filter((element)=> element.price > 5);
-console.log(arr);
-
-//Increase all prices by 10% (GST)
-let upadated = cart.map(item =>{
-    return {
-        name:item.name,
-           price :item.price * 1.10
-    };
+let sc = [
+  { name: "A", score: 88 },
+  { name: "B", score: 95 },
+  { name: "C", score: 90 }
+]
+const v =sc.reduce((acc,current)=>{
+    return acc.score < current.score ? current : acc;
 });
-console.log(upadated);
+console.log(v);
 
 
-let students = [
-  { name: "A", marks: 35 },
-  { name: "B", marks: 80 },
-  { name: "C", marks: 60 },
-  { name: "D", marks: 20 }
-];
-//Get only passed students (>= 40)
-const arr2 = students.filter((item) => item.marks > 40);
-console.log(arr2);
-
-// Add grade property based on marks
-// Rules:
-// =75 → Distinction
-
-// =60 → First
-
-// =40 → Pass
-
-// else → Fail
-
-let grade = students.map(item =>{
-    return{ marks : item.marks=75
-
+num = [1,2,3,4,5,6];
+const result = num.reduce((acc,current)=>{
+    if(current % 2 === 0){
+acc.even.push(current);
     }
-    console.log(grade);
-    
-})
+    else{
+        acc.odd.push(current);
+    }
+    return acc;
+},{even:[], odd:[]});
+console.log(result);
